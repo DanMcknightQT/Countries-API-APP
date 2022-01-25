df9f6e4e55f88d04b43a88a5f761f4168e5d8d01
Darrell Green <darrellgreendavis@yahoo.com>
2022-01-24 22:16:35 -0500

    populates all cards with proper info

--- js.js
+++ js.js
@@ -1,11 +1,28 @@
 (function(){
     const URL = 'https://restcountries.com/v2/all'
-    
+
     $.ajax({
         type: "GET",
         url: `${URL}`,
         success: (data) =>{
-            console.log(data)
+            console.log(data, 'data from get call')
+            Object.values(data).forEach(country =>{
+                console.log(country.flags.png, 'flag png')
+                
+
+                let contentBox =
+                    `<section class="countryCard">
+                    <img id="country-Flag" src="${country.flags.png}">
+                    <h1 id="country-Name">${country.name}</h1>
+                    <article class="cardInfo">
+                    <p><span class="infoTitle">Population:</span> ${country.population}</p>
+                    <p><span class="infoTitle">Region:</span> ${country.region}</p>
+                    <p><span class="infoTitle">Capital:</span> ${country.capital}</p>
+                    </article>
+                    </section>`
+
+                $('#cards-Div').append(contentBox)
+            })
         },
         error: (error) =>{
             console.log(error)
@@ -13,4 +30,6 @@
     })
 
 })();
+let countryInfo = []
+//console.log(countryInfo, 'Country Info Array')
 
