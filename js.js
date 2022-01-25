dd77bd12d4676b756a7617106375ccab2dfaad78
Darrell Green <darrellgreendavis@yahoo.com>
2022-01-25 00:11:15 -0500

    save, data is structured

--- js.js
+++ js.js
@@ -1,21 +1,39 @@
 (function(){
     const URL = 'https://restcountries.com/v2/all'
 
+    function formatNumber(num) {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+    }
+
     $.ajax({
         type: "GET",
         url: `${URL}`,
         success: (data) =>{
-            console.log(data, 'data from get call')
             Object.values(data).forEach(country =>{
-                console.log(country.flags.png, 'flag png')
-                
+                let population = formatNumber(country.population)
+                //console.log(country)
+                let countryObj = {
+                    [country.name]: {
+                        nativeName: country.nativeName,
+                        population: population,
+                        region: country.region,
+                        subRegion: country.subRegion,
+                        capital: country.capital,
+                        topLevelDomain: country.topLevelDomain,
+                        currencies: country.currencies,
+                        languages: country.languages,
+                        borderCountries: country.borders
+                    }
+                }
+
+                countryInfo.push(countryObj)
 
                 let contentBox =
                     `<section class="countryCard">
-                    <img id="country-Flag" src="${country.flags.png}">
+                    <img onclick="detailedView(this)" id="country-Flag" src="${country.flags.png}">
                     <h1 id="country-Name">${country.name}</h1>
                     <article class="cardInfo">
-                    <p><span class="infoTitle">Population:</span> ${country.population}</p>
+                    <p><span class="infoTitle">Population:</span> ${population}</p>
                     <p><span class="infoTitle">Region:</span> ${country.region}</p>
                     <p><span class="infoTitle">Capital:</span> ${country.capital}</p>
                     </article>
@@ -31,5 +49,20 @@
 
 })();
 let countryInfo = []
+
 //console.log(countryInfo, 'Country Info Array')
 
+function detailedView(country){
+    $('.totalView').addClass('hide');
+    country = country.parentElement.childNodes[3].innerHTML
+    //console.log(country)
+    Object.values(countryInfo).forEach(value =>{
+        //console.log(value)
+        Object.entries(value).forEach(([key, value]) =>{
+            if(country === key){
+                console.log(key, 'key')
+                console.log(value, 'value')
+            }
+        })
+    })
+}
