b03544ce23fcf80d830fd174acc395ed59e5fa8f
Darrell Green <darrellgreendavis@yahoo.com>
2022-01-25 01:26:35 -0500

    still need to do border countries on mobile

--- js.js
+++ js.js
@@ -17,12 +17,13 @@
                         nativeName: country.nativeName,
                         population: population,
                         region: country.region,
-                        subRegion: country.subRegion,
+                        subRegion: country.subregion,
                         capital: country.capital,
                         topLevelDomain: country.topLevelDomain,
                         currencies: country.currencies,
                         languages: country.languages,
-                        borderCountries: country.borders
+                        borderCountries: country.borders,
+                        flag: country.flags.png
                     }
                 }
 
@@ -53,16 +54,59 @@ let countryInfo = []
 //console.log(countryInfo, 'Country Info Array')
 
 function detailedView(country){
+    let currencyName = null;
+    let languages = [];
     $('.totalView').addClass('hide');
+    $('.focusedView').removeClass('hide');
     country = country.parentElement.childNodes[3].innerHTML
     //console.log(country)
     Object.values(countryInfo).forEach(value =>{
-        //console.log(value)
+       //console.log(value)
         Object.entries(value).forEach(([key, value]) =>{
             if(country === key){
                 console.log(key, 'key')
                 console.log(value, 'value')
+                Object.values(value.currencies).forEach(currency =>{
+                    currencyName = currency.name
+                })
+                Object.values(value.languages).forEach(language =>{
+                    languages.push(language.name)
+                })
+                
+
+                let content =
+                `<div id="target">
+                <section class="largerImage" >
+                    <img src="${value.flag}">
+                </section>
+
+                <h1 id="country-NameLg">${key}</h1>
+                <article class="cardInfoLg">
+                    <p><span class="infoTitle">Native Name:</span> ${value.nativeName}</p>
+                    <p><span class="infoTitle">Population:</span> ${value.population}</p>
+                    <p><span class="infoTitle">Region:</span> ${value.region}</p>
+                    <p><span class="infoTitle">Sub Region:</span> ${value.subRegion}</p>
+                    <p><span class="infoTitle">Capital:</span> ${value.capital}</p>
+                </article>
+                <br>
+                <article class="cardInfoLg">
+                    <p><span class="infoTitle">Top Level Domain:</span> ${value.topLevelDomain}</p>
+                    <p><span class="infoTitle">Currencies:</span> ${currencyName}</p>
+                    <p><span class="infoTitle">Languages:</span> ${languages.join(", ")}</p>
+                </article>
+                </div>`
+
+                $('#single-Card').append(content)
             }
         })
     })
+    console.log(languages)
+}
+
+function goBack(){
+    $('.totalView').removeClass('hide');
+    $('.focusedView').addClass('hide');
+    $('#target').remove();
+    $(window).scrollTop(0);
+
 }
